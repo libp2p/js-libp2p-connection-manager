b4feaec818891c8b90d81d4a22bafcc675026292
Jacob Heun <jacobheun@gmail.com>
2019-04-10 20:17:32 +0200

    chore: fix linting

--- test/utils/create-libp2p-node.js
+++ test/utils/create-libp2p-node.js
@@ -42,7 +42,7 @@ function createLibp2pNode (options, callback) {
   let node
 
   waterfall([
-    (cb) => PeerId.create({bits: 1024}, cb),
+    (cb) => PeerId.create({ bits: 1024 }, cb),
     (id, cb) => PeerInfo.create(id, cb),
     (peerInfo, cb) => {
       peerInfo.multiaddrs.add('/ip4/127.0.0.1/tcp/0')
